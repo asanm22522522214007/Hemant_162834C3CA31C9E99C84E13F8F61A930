cd4d6ff833f3504b60b0b971dd26423f903fe1d5
asanm22522522214007 <144426436+asanm22522522214007@users.noreply.github.com>
2023-09-19 13:28:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-year =int(input("Enter year"))
-if(year % 4==0 and year % 100 !=0) or(year % 400 ==0):
-  print (year,'it is leap year')
 
-else:
-  print (year,'is not leap year')
+class BankAccount:
+
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print("Deposit ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print("Withdraw ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print("Account balance for {} (account #{}): ₹{}".format(
+            self.__account_holder_name, self.account_number,
+            self.__account_balance))
+
+account = BankAccount(account_number="123456789",
+                      account_holder_name="cibi",
+                      initial_balance=100000.0)
+
+account.display_balance()
+account.deposit(50000.0)
+account.withdraw(20000.0)
+account.display_balance()
